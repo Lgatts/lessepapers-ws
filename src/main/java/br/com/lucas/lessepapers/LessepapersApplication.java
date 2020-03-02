819adc80ea9ef1d5690882621f65a00950bf2517
Lgatts <Lgatts@gmail.com>
2020-03-02 18:36:38 -0300

    Project Base

---
+++ src/main/java/br/com/lucas/lessepapers/LessepapersApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.lessepapers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessepapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessepapersApplication.class, args);
+	}
+
+}
